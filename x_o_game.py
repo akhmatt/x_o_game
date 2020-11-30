525c95e5e797c88cb5ca1154734261f11e924d5e
akhmatt <62340881+akhmatt@users.noreply.github.com>
2020-11-30 20:32:40 +0300

    Update x_o_game.py

--- x_o_game.py
+++ x_o_game.py
@@ -2,7 +2,7 @@ board = list(range(1, 10))
 
 def show_board():  # отображение поля игры
     for i in range(3):
-        print(board[0+i*3], board[1+i*3], board[2+i*3])
+        print(board[0+i*3], board[1+i*3], board[2+i*3]) #выводит в консоль по 3 элемента из списка board на каждом шагу  цикла for
 
 
 
@@ -13,7 +13,7 @@ def take(choise): # ввод
             print('Неверный ввод. Введите заново')
             continue
         value = int(value)
-        if str(board[value - 1]) in 'XO':
+        if str(board[value - 1]) in 'XO': # проверка на занятость клетки
             print("место занято")
             continue
         board[value-1] = choise
@@ -23,14 +23,14 @@ wins_vars = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (
 
 def win(): # проверка выигрыша
     for j in wins_vars:
-        if board[j[0] - 1] == (board[j[1] - 1]) == (board[j[2] - 1]):
+        if board[j[0] - 1] == (board[j[1] - 1]) == (board[j[2] - 1]): #сначала я тут все три случая взял в скобки и игра запускалась но не понимала выигрышей, поискал в инете и понял что проверяется на равенство первая и вторая позиция (board[j[0] - 1] == (board[j[1] - 1])). Выражение принимает значения true or false соответственно, а затем результат, то есть одно из булевых значений, сравнивается с третьей позицией, поэтому программа не понимает, когда кто-то выиграл.
             return board[j[1]-1]
     else:
         return False
 
 
 def main_func():
-    counter = 0
+    counter = 0 # переменная подсчета ходов 
     while True:
         show_board()
         if counter % 2 == 0:
